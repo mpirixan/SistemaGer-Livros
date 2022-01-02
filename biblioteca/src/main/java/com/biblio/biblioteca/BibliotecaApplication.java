35b33e575c6e0e5bc73b7126036ad91eed3eb4a3
mpirixan <mpirixan@yahoo.com>
2022-01-02 15:23:09 -0300

    Programa inicial

--- biblioteca/src/main/java/com/biblio/biblioteca/BibliotecaApplication.java
+++ biblioteca/src/main/java/com/biblio/biblioteca/BibliotecaApplication.java
@@ -2,8 +2,16 @@ package com.biblio.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@OpenAPIDefinition(info = @Info(description = "API - Sua Conta Basa", contact = @Contact(name = "Matheus Pirixan", email = "mpirixan@yahoo.com")))
+
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
--- biblioteca/src/main/java/com/biblio/biblioteca/domain/model/Cliente.java
+++ biblioteca/src/main/java/com/biblio/biblioteca/domain/model/Cliente.java
@@ -1,10 +1,18 @@
 package com.biblio.biblioteca.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="Cliente")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="idcliente")
+	private Long idCliente;
 }
