a94d0783a39c04856614f4c27fe320d5602991d0
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2022-01-03 10:28:42 -0300

    Adição de atributos

--- biblioteca/src/main/java/com/biblio/biblioteca/domain/model/Cliente.java
+++ biblioteca/src/main/java/com/biblio/biblioteca/domain/model/Cliente.java
@@ -1,5 +1,7 @@
 package com.biblio.biblioteca.domain.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,4 +17,65 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="idcliente")
 	private Long idCliente;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="usuario", unique = true)
+	private String usuario;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Long idCliente, String nome, String usuario) {
+		super();
+		this.idCliente = idCliente;
+		this.nome = nome;
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idCliente, nome, usuario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(idCliente, other.idCliente) && Objects.equals(nome, other.nome)
+				&& Objects.equals(usuario, other.usuario);
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	
 }
