218b60ece9b73d4f53c88ac54d1e79bd7e6b33ee
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2022-01-06 13:42:44 -0300

    Security com senha criptografa - dando 403 forbiden

--- biblioteca/src/main/java/com/biblio/biblioteca/controllers/UsuarioController.java
+++ biblioteca/src/main/java/com/biblio/biblioteca/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,7 @@ public class UsuarioController {
 	}
 	
 	@PostMapping(value = "/cadastro-usuario")
+	@PreAuthorize("hasRole('ADMIN')")
 	public UsuarioDTO insert(@RequestBody UsuarioDTO obj) {
 		return modelMapper.map(services.insert(obj),UsuarioDTO.class);
 	}
--- biblioteca/src/main/java/com/biblio/biblioteca/controllers/services/UsuarioServices.java
+++ biblioteca/src/main/java/com/biblio/biblioteca/controllers/services/UsuarioServices.java
@@ -3,6 +3,7 @@ package com.biblio.biblioteca.controllers.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +16,7 @@ import com.biblio.biblioteca.repository.UsuarioRepository;
 @Validated
 public class UsuarioServices {
 
+	
 	@Autowired
 	private UsuarioRepository repository;
 	
@@ -24,10 +26,11 @@ public class UsuarioServices {
 	
 	@Transactional
 	public Usuario insert(UsuarioDTO obj) {
+		BCryptPasswordEncoder passEncode = new BCryptPasswordEncoder();
 		Usuario cliente = new Usuario();
 		cliente.setNome(obj.getNome());
 		cliente.setApelidoUsuario(obj.getApelidoUsuario());
-		cliente.setPassword(obj.getPassword());
+		cliente.setPassword(passEncode.encode(obj.getPassword()));
 		cliente.setEmail(obj.getEmail());;
 		return repository.save(cliente);
 	}
