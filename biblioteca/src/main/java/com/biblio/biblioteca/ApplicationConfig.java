b08eab7b8b3a59107dcd12dbd08b08adca6e6e27
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2022-01-06 13:09:24 -0300

    Security basic auth em memoria

--- biblioteca/src/main/java/com/biblio/biblioteca/ApplicationConfig.java
+++ biblioteca/src/main/java/com/biblio/biblioteca/ApplicationConfig.java
@@ -1,11 +1,21 @@
 package com.biblio.biblioteca;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@EnableWebSecurity
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig extends WebSecurityConfigurerAdapter{
+	
 	
     @Bean
     public ModelMapper modelMapper() {
@@ -14,5 +24,26 @@ public class ApplicationConfig {
     }
     
     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests()
+    		.anyRequest()
+    		.authenticated()
+    		.and()
+    		.httpBasic();
+    }
+    
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    	
+    	PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    	auth.inMemoryAuthentication()
+    		.withUser("sistema")
+    		.password(passwordEncoder.encode("sistema"))
+    		.roles("ADMIN");
+    	
+    	super.configure(auth);
+    }
  
 }
